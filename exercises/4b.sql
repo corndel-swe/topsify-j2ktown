49d4e1b14e7f90c5f388b64ed5b395d8f6f7cdd9
James Kett <james2kett@gmail.com>
2024-10-08 13:34:46 +0100

    day 3 exercises sql

--- exercises/4b.sql
+++ exercises/4b.sql
@@ -1,3 +1,11 @@
 -- List the 10 slowest tracks in the database
 -- Order by tempo, with the slowest first
 -- Include the track id, track title and track tempo
+
+-- what tables columsn do i need - tracks.id, tracks.name, features.tempo
+.mode markdown
+select tracks.id, tracks.name, features.tempo
+from tracks
+inner join features on tracks.id = features.track_id
+order by tempo
+
--- exercises/5a.sql
+++ exercises/5a.sql
@@ -1,1 +1,5 @@
 -- How many tracks are longer than 5 minutes?
+.mode markdown
+select count(id)
+from tracks
+where tracks.duration_ms > 300000
