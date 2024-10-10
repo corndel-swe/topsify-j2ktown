251be7c7ca3cede006152e68a2ba6f6330d95a78
James Kett <james2kett@gmail.com>
2024-10-10 11:37:43 +0100

    Completed exercises day3 sql

--- exercises/6a.sql
+++ exercises/6a.sql
@@ -2,7 +2,7 @@
 -- Include a count of how many albums they have released
 -- Order your results by artist name in ascending order
 -- (You should group the artists by id, not by name)
-.mode markdown
+
 select artists.name, count(albums.artist_id)
 FROM artists
 INNER JOIN albums ON artists.id = albums.artist_id
